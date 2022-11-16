9c22ba12bb1c659ad8710efc3ddb2ed83d408856
Joe Garvey <jmg237@cam.ac.uk>
2022-11-16 10:19:48 +0000

    Added Front IR Sensor

--- robot_main/robot_main.ino
+++ robot_main/robot_main.ino
@@ -1,20 +1,30 @@
 /// Libraries
 #include <Adafruit_MotorShield.h>
-
+// README
+// Pin Definitions
+#define IRPin A0
+#define Line_Left_Sensor A1
+#define Line_Right_Sensor A2
+#define Proximity_Front_LED 8
 // README
 // Left Line Sensor = A1
 // Right Line Sensor = A2
-// Left Motor = Port 1
-// Right Motor = Port 2
+// IR Front Sensor = A3
+// Left Motor = Port 3
+// Right Motor = Port 4
+// Front Proximity = 13
 
 /// Variable Declaration
 const int slow = 50;
-const int fast = 250;
+const int fast = 150;
 float Line_Left;
 float Line_Right;
-float Line_Threshold = 20.0;
+int Line_Threshold = 60;
+float IR_Front;
+int IR_Threshold = 350;
 uint8_t Left_Motor_Speed = slow;
 uint8_t Right_Motor_Speed = slow;
+int cycles_deviated = 0;
 
 /// Motor Shield Setup
 Adafruit_MotorShield AFMS = Adafruit_MotorShield(); // Create the motor shield object with the default I2C address
@@ -24,6 +34,8 @@ Adafruit_DCMotor *Right_Motor = AFMS.getMotor(2); // And Right to Port 2
 /// INITIAL SETUP
 void setup() {
   Serial.begin(9600);
+  pinMode(IRPin, INPUT);
+  pinMode(Proximity_Front_LED, OUTPUT);
   // Ensure Motor Shield is connected to Arduino.
   while(!AFMS.begin()){
         Serial.println("Could not find Motor Shield. Check wiring.");
@@ -36,13 +48,18 @@ void setup() {
 
 /// Reads Line Sensors, Outputs to Serial
 void ReadLineSensor(){
-  Line_Left = analogRead(A1);
-  Line_Right = analogRead(A2);
+  Line_Left = analogRead(Line_Left_Sensor);
+  Line_Right = analogRead(Line_Right_Sensor);
   //Serial.println("L / R Line Sensors");
   Serial.print(Line_Left);
   Serial.print(" ");
   Serial.print(Line_Threshold);
   Serial.print(" ");
+  ///
+  IR_Front = analogRead(IRPin);
+  Serial.print(IR_Front);
+  Serial.print(" ");
+  ///
   Serial.println(Line_Right);
   //Serial.println("L/R Sensor Output");
 }
@@ -51,6 +68,7 @@ void ReadLineSensor(){
 
 void Move_Straight(){
   //Serial.print("STRAIGHT");
+  cycles_deviated = 0;
   if(Left_Motor_Speed != fast) { 
     Left_Motor->setSpeed(fast); 
     Left_Motor_Speed = fast;
@@ -104,7 +122,8 @@ void loop() {
   else if (Line_Left < Line_Threshold && Line_Right > Line_Threshold){ Move_Right(); }
   else { Move_Lost(); }
   // TODO test if setting motor speed here or in the function is faster? Use of two variable versus one
-  //if(Left_Motor_Speed )
   Left_Motor -> setSpeed(Left_Motor_Speed);
   Right_Motor -> setSpeed(Right_Motor_Speed);
+  if(IR_Front > IR_Threshold){ digitalWrite(Proximity_Front_LED, HIGH); }
+  else { digitalWrite(Proximity_Front_LED, LOW); }
 }
